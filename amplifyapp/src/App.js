63df88d4189abf3a82597a5f8205925ea026e289
Jose Portillo Cruz <jose.portillo@globant.com>
2021-07-21 14:51:24 -0500

    test50

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Juver!! :D.
+          Hello Juver!! :D  It's Work.
         </p>
         <a
           className="App-link"
